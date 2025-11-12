4b2b7e74996027b786623f233693412422d3398e
Fowyous <mohamed.koubaa@utbm.fr>
2025-11-12 11:02:38 +0100

    Create projet.h

---
+++ projet.h
@@ -0,0 +1,20 @@
+typedef struct premisse { 
+        char * conclusion;
+        struct premisse next;
+}premisse;
+
+typedef struct { 
+
+  premisse 
+}Regle;
+
+typedef struct { 
+
+
+
+}BC;
+
+
+Regle Creer_regle();
+Regle Ajout_premisse();
+Rgle Conclusion()
