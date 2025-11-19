69e16fd1a27eb30b79e46a443999cd12321ba316
Fowyous <mohamed.koubaa@utbm.fr>
2025-11-19 21:32:15 +0100

    Create gestion_regle.c
    mettre le code qui etait dans q 1et2 en c avec pseudo code.c

---
+++ gestion_regle.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+///////////////////////////////////////////////////////////////
+//             FONCTIONS DE GESTION DES REGLES               //
+///////////////////////////////////////////////////////////////
+Regle Creer_regle(){
+    Regle new_regle;
+    new_regle.conclusion = NULL;
+    new_regle.premisse = NULL;
+    return new_regle;
+}
+void Conclusion(Regle* regle, char* conclusion){
+    if (regle->conclusion != NULL) {
+        regle->conclusion = conclusion;
+    }
+}
+
+bool est_vide(Propositions* premisse){
+    Propositions* p = premisse;
+    if (p->proposition != NULL)
+        return false;
+    return true;
+}
+
+void Ajout_proposition(Regle* regle, char* proposition){
+    Propositions* new_propo = (Propositions*)malloc(sizeof(Propositions));
+    new_propo->proposition = proposition;
+    new_propo->next = NULL;
+
+    if (est_vide((regle->premisse))) {
+        regle->premisse = new_propo;
+    } else {
+        Propositions* current = regle->premisse;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_propo;
+    }
+}
+
+char* conclusion_regle(Regle* regle){
+    char* c = regle->conclusion;
+    if (c == NULL) {
+        return NULL; // ou afficher un message d'erreur
+    } else {
+        return c;
+    }
+}
+
+char* tete_premisse(Propositions* premisse){
+    Propositions* p = premisse;
+    if (est_vide(p)) {
+        return NULL; // ou afficher un message d'erreur
+    } else {
+        return p->proposition;
+    }
+}
+
+bool est_dans_premisse(Propositions* premisse, char* ptest){
+    Propositions* p = premisse;
+    if (est_vide(p)) {
+        return false;
+    } else if (strcmp(p->proposition, ptest) == 0) {
+        return true;
+    } else if (p->next != NULL) {
+        return est_dans_premisse(p->next, ptest);
+    } else {
+        return false;
+    }
+}
+
