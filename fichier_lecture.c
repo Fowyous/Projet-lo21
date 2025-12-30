9bd9b346a20dc8c9534b98c60b57ac1a326fdd81
Fowyous <mohamed.koubaa@utbm.fr>
2025-12-30 19:59:54 +0100

    la lecture du fichier est prète mais l'ecriture pas encore

--- fichier_lecture.c
+++ fichier_lecture.c
@@ -2,7 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 
-BC *lecture_fichier(char *chemin){//lis les bases de connaissances dans le fichier et retourne la tête.
+BC *Sauvegarde(BC *base, const char *chemin){
+	FILE *file;
+	file = fopen(chemin, "w");
+        if (file == NULL){
+                printf("Ouverture du fichier impossible!!\n");
+                exit(1);
+        }
+	while (base){
+
+
+	}
+
+}
+BC *lecture_fichier(const char *chemin){//lis les bases de connaissances dans le fichier et retourne la tête.
 	FILE *file;
 	char ligne[BUFFER];
 
@@ -12,110 +25,41 @@ BC *lecture_fichier(char *chemin){//lis les bases de connaissances dans le fichi
                 printf("Ouverture du fichier impossible!!\n");
                 exit(1);
         }
-	//BC *bc_tete =  Creer_BC(NULL);
-	//BC *bc_queue = bc_tete;
-	BC *bc_tete = NULL;
-	if (fgets(ligne, sizeof(ligne), file) != NULL){
-		// le fichier ne dois pas contenir de \n
-		ligne[strcspn(ligne, "\n")] = 0;
-		// les bases sont séparées par , la variable bc_data contient toutes les infos necessaires pour remplire une base
-		char *bc_data = strtok(ligne, ",");
-		printf("bc_data : %s\n", bc_data);
-
-		while (bc_data != NULL){
-			//Créer une nouvelle base
-			BC *nouvel_bc = Creer_BC(bc_tete);
-			bc_tete = nouvel_bc;
-			
-			//Extraire le nom d'une base.
-			char *bc_nom = strtok(bc_data, ";");
-			nouvel_bc->nom = strdup(bc_nom);
-			printf("bc_nom : %s\n", bc_nom);
-			//Traitement de la régle dans la base
-			char *regle_data = strtok(NULL, ",");
-			printf("regle_data : %s\n", regle_data);
-			while(regle_data != NULL){
-				//Extraire une régle et la diviser en conclusion et proposition
-				char *conclusion = strtok(regle_data, ":");
-				printf("conclusion : %s\n", conclusion);//enl
-				if (conclusion != NULL){		
-					Regle* nouvel_regle = Creer_regle();
-					Conclusion(nouvel_regle, conclusion);
-
-					//Extraire les propositions
-					char *propositions = strtok(NULL, ";");
-					printf("propositions : %s\n", propositions);//enl
-					if (propositions != NULL){	
-						char *proposition = strtok(propositions, ":");
-						while (proposition != NULL){
-					printf("propositions : %s\n", propositions);//enl
-							Ajout_proposition(nouvel_regle, proposition);
-							proposition = strtok(NULL, ":");
-						}
-					}
-				
-					//Ajouter la régle a la base.
-					Ajout_regle(nouvel_bc, nouvel_regle);
-				}
-				
-				//Passer a la prochaine régle (si elle existe) dans la base
-				regle_data = strtok(NULL, ";");
-				printf("regle_data2 : %s\n", regle_data);
-			}
-
-			//Passer a la prochaine base
-			bc_data = strtok(NULL, ",");
+	
+	BC *tete_base = NULL;
+	BC *base = tete_base;
+	
+	Regle * nouvel_regle;
+	while (fgets(ligne, sizeof(ligne), file) != NULL){
+		if (strncmp(ligne, "BC:", 3) == 0) {//ligne contiendera seulement le nom de la base
+			base = Creer_BC(tete_base);
+			char * nom_bc = strdup(ligne + 4);// on enleve BC: du nom
+			nom_bc[strlen(nom_bc) - 1] = '\0';//on enleve le \n
+			Ajout_nom_BC(base, nom_bc);
 
 		}
-
-
-
-/*
-		int len = strlen(bc_data);
-		bc_data[len] = ';';
-		printf("bc_data : %s\n", bc_data);
-		bc_data[len + 1] = '\0';
-		printf("bc_data : %s\n", bc_data);
-		
-		char * mot = strtok(bc_data, ";");
-		// le premier mot c'est le nom de la base
-		if (mot) {
-			Ajout_nom_BC(bc_queue, mot);
+		else if (strncmp(ligne, "REGL:", 5) == 0){
+			nouvel_regle = Creer_regle();
+			char * conclusion = strdup(ligne + 6);// on enleve REGL:
+			conclusion[strlen(conclusion) - 1] = '\0';//on enleve le \n
+			Conclusion(nouvel_regle, conclusion);
+			Ajout_regle(base, nouvel_regle);
 		}
-		char* regle_data = strtok(NULL, ";");
-		Regle *nouvel_regle = Creer_regle();// le premier mot c'est la conclusion
-		while (regle_data){
-
-			printf("regle_data : %s\n", regle_data);
-			getchar();
-			mot = strtok(regle_data, ":");
-			if (mot) {
-				Conclusion(nouvel_regle, mot);
-			}	
-			while (mot){//c'est la boucle pour les régles(condition a regler)
-				mot = strtok(NULL, ":");
-				printf("mot : %s\n", mot);
-				getchar();
-				if (mot){
-					Ajout_proposition(nouvel_regle, mot);
-				}
-			}
-			//regle_data = strtok(bc_data, regle_data);
-			//printf("regle_data2 : %s\n", regle_data);
-			regle_data = strtok(NULL, ";");
-			
-			printf("regle_data3 : %s\n", regle_data);
-			getchar();
-
+		else if (strncmp(ligne, "    PROPOS:", 11) == 0){
+			char *proposition = strdup(ligne + 12);// on enleve le 	PROPOS:
+			proposition[strlen(proposition) - 1] = '\0';//on enleve le \n
+			Ajout_proposition(nouvel_regle, proposition);
 		}
-		Ajout_regle(bc_queue, nouvel_regle);
-		bc_queue = Creer_BC(bc_tete);*/
+	
 	}
+
 	fclose(file);
-	return bc_tete;
+	return tete_base;
 }
 
 int main(){
 	lecture_fichier("bases.txt");
 
 }
+
+
--- main.h
+++ main.h
@@ -23,7 +23,7 @@ typedef struct Regle {
 	struct Regle* next;
 }Regle;
 
-///////////////////////////////////chaine de regles///il faut ajouter le nom de la base
+///////////////////////////////////chaine de regles
 typedef struct BC{
         char* nom;
         Regle* regle;
@@ -34,7 +34,8 @@ typedef struct BC{
 //_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_//fonctions//¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\\
 //¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_/¯\_//
 
-Regle* Creer_regle();        //cree une regle vide
+Regle *Creer_regle();        //cree une regle vide
+Regle *Regle_suivant(Regle *regle);//a regler!
 void Ajout_proposition(Regle* regle, char* proposition);        //ajout une proposition a la premisse d une regle en queu
 void Conclusion(Regle* regle, char* conclusion);        //cree la conclusion d une regle
 bool est_dans_premisse(Propositions* premisse, char* ptest);        //Tester si une proposition appartient à la prémisse d’une règle, de manière récursive
@@ -50,6 +51,11 @@ BC* Creer_BC(BC* bases);        //cree une base vide a la queue d'une liste de b
 void Ajout_regle(BC *base, Regle* regle);        //ajout en queu la regle dans la BC
 Regle* tete_Base(BC *base);        //acceder a la regle se trouvant la tete de la base
 
-BC *lecture_fichier(char *chemin);
+BC *bc_suivant(BC *base);//a regler!
+
+
+BC *lecture_fichier(const char *chemin);
+BC *sauvegarde(BC *base, const char *chemin);
 void Ajout_nom_BC(BC* base, char* nom);
+
 #endif
