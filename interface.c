265f56b58c6d2835ed3a02ea503d41520019d6b4
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-03 15:44:12 +0100

    Update interface.c

--- interface.c
+++ interface.c
@@ -63,33 +63,121 @@ void get_base _numero(int numero){
 
 int main() {
     char choix;
+    int num_regle;
+    int conttinuer = 1;
+    int selection = 1;
+    int action = 1;
+    BC *bases = *lecture_fichier(const char *chemin);
     while (conttinuer == 1){
+        ///choix de la base
         while (selection == 1){
             printf("Voulez vous choisir une base ou en creer une nouvelle? (s: selectioner une base, n: nouvelle base)\n");
             scanf(" %c", &choix);
             if (choix == 's'){
-                BC* base = lister_base(NULL);
-                liste_regles_base(base);
+                BC* b = lister_base(*bases);
+                liste_regles_base(b);
                 selection = 0;
             }
-    
+
             else if (choix == 'n'){
                 printf("Creation de nouvelle base non implementee.\n");
-                b = create_base();
+                b = Creer_BC(*bases);
                 printf("Entrez le nom de la nouvelle base : ");
                 char nom_base[100];
                 scanf("%s", nom_base);
                 b->nom = nom_base;
                 printf("Base cree : %s\n", b->nom); 
-                ///ajouter la base a la liste des bases
                 selection = 0;
             }
-    
+
+            else {
+                printf("Choix invalide.\n");
+            }            
+        }
+
+        ///choix de l'action a effectuer sur la base
+        while (action == 1){
+            printf("Voulez vous effectuer une action sur cette base? (o: oui, n: non ferme programme, c: changer de base)\n");
+            scanf(" %c", &choix);
+            if (choix == 'o'){
+                printf("choisissez une action a effectuer sur cette base\n");
+                printf("a: ajouter une regele\n");
+                printf("b: modifier une regle\n");
+                printf("c: supprimer une regle\n");
+                printf("d: afficher les regles\n");
+                printf("e: supprimer la base\n");
+                scanf(" %c", &choix);
+                switch (choix){
+                    case 'a':
+                        printf("Ajouter une regle non implemente.\n");
+                        break;
+
+                    case 'b':
+                        printf("quel numero de regle voulez-vous modifier?\n");
+                        scanf("%d", &num_regle);
+                        printf("quel modification voulez-vous effectuer?\n");
+                        printf("modifier la premisse (p), modifier la conclusion (c)\n");
+                        scanf(" %c", &choix);
+                        if (choix == 'p'){
+                            printf("Modifier la premisse non implemente.\n");
+                        }
+                        else if (choix == 'c'){
+                            printf("Modifier la conclusion non implemente.\n");
+                        }
+                        else {
+                            printf("Choix invalide.\n");
+                        }
+                        
+                        break;
+
+                    case 'c':
+                        printf("Supprimer une regle non implemente.\n");
+                        supr_Regle(BC *base);
+                        break;
+
+                    case 'd':
+                        liste_regles_base(b);
+                        break;
+
+                    case 'e':
+                        printf("la base a ete supprimee.\n");
+                        *supr_bc(b);
+                        action = 0;
+                        selection = 1;
+                        break;
+
+                    default:
+                        printf("Choix invalide.\n");
+                        break;
+                }
+                action = 0;
+            }
+
+            else if (choix == 'n'){
+                conttinuer = 0;
+                action = 0;
+            }
+
+            else if (choix == 'c'){
+                selection = 1;
+                action = 0;
+            }
+
             else {
                 printf("Choix invalide.\n");
             }
-        }
 
-        return 0;
+        ///sauvegarde des bases avant de quitter
+        printf("voulez vous souvgarder?(o: oui, n: non)\n");
+        scanf(" %c", &choix);
+        if (choix == 'o'){
+            *Sauvegarde(*bases, /*chemin a modif */);
+            printf("Bases sauvegardees.\n");
+        }
+        else if (choix == 'n'){
+            printf("Bases non sauvegardees.\n");
+        }
+         
     }
+    return 0;
 }
