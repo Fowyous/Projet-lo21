5a1d4d53f87940a795a79feefe1d9697357647c2
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-05 20:19:41 +0100

    Update main.c

--- main.c
+++ main.c
@@ -90,7 +90,229 @@ void moteur_inference(BC* base){
 
 
 
+void principal() {
+    char choix;
+    int num_regle;
+    int conttinuer = 1;
+    int selection = 1;
+    int action = 1;
+    int taille = 0;
+    const char *chemin = "bases.txt";       /////chemin a modif
+    BC *bases = lecture_fichier(chemin);        /////chemin a modif
+    BC *b = NULL;
+    while (conttinuer == 1){
+        ///choix de la base
+        while (selection == 1){
+            printf("Voulez vous choisir une base ou en creer une nouvelle? (s: selectioner une base, n: nouvelle base)\n");
+            choix = getchar();
+            if (choix == 's'){
+                b = choisir_base(bases);
+                liste_regles_base(b);
+                taille = taille_base(b);
+                selection = 0;
+            }
 
+            else if (choix == 'n'){
+                printf("Creation de nouvelle base non implementee.\n");
+                b = Creer_BC(bases);
+                printf("Entrez le nom de la nouvelle base : ");
+                char nom_base[100];
+                scanf("%s", nom_base);
+                b->nom = nom_base;
+                printf("Base cree : %s\n", nom_bc(b)); 
+                selection = 0;
+            }
+
+            else {
+                printf("Choix invalide.\n");
+            }            
+        }
+
+        ///choix de l'action a effectuer sur la base
+        while (action == 1){
+            printf("Voulez vous effectuer une action sur cette base? (o: oui, n: non ferme programme, c: changer de base)\n");
+            choix = getchar();
+            if (choix == 'o'){
+                printf("choisissez une action a effectuer sur cette base\n");
+                printf("a: ajouter une regele\n");      ///a verif
+                printf("b: modifier une regle\n");      ///a verif
+                printf("c: supprimer une regle\n");     ///a verif
+                printf("d: afficher les regles\n");     ///a verif
+                printf("e: supprimer la base\n");       ///a verif
+                printf("f: sauvegarder les bases\n");   ///a verif
+                printf("g: moteur inference\n");        ///a verif
+                choix = getchar();
+                switch (choix){
+                    ///ajout d'une regle
+                    case 'a':
+                        int ajout = 1;
+                        Regle* r = creer_regle();
+                        printf("Entrez la conclusion de la regle : ");
+                        char conclusion[100];
+                        scanf("%s", conclusion);
+                        r->conclusion = conclusion;
+                        while (ajout == 1)
+                            printf("Entrez la proposition de la regle ou f quand vous avez fini: ");
+                            char propo[100];
+                            scanf("%s", propo);
+                            if (strcmp(propo, "f") == 0){
+                                printf("Regle cree :\n");
+                                afficher_regle(r);
+                                Ajout_regle(b, r);
+                                ajout = 0;
+                                break;
+                            }
+                            else {
+                                Ajout_proposition(r, propo);
+                            }
+
+                        
+                        break;
+
+                    ///modification d'une regle
+                    case 'b':
+                        printf("quel numero de regle voulez-vous modifier?\n");
+                        scanf("%d", &num_regle);
+                        if (num_regle > taille || num_regle <= 0){
+                            printf("Numero de regle invalide.\n");
+                            break;
+                        }
+                        else{
+                            int modif = 1;
+                            while (modif == 1){
+                                printf("quel modification voulez-vous effectuer?\n");
+                                r = get_regle_numero(b, num_regle);
+                                printf("modifier la premisse (p), modifier la conclusion (c)\n");
+                                choix = getchar();
+                                ///modification de la premisse
+                                if (choix == 'p'){
+                                    printf("a: ajout, s: suppression\n");
+                                    choix = getchar();
+                                    if (choix == 'a'){
+                                        printf("nouvelle premisse a ajouter\n");
+                                        char new_premisse[100];
+                                        scanf("%s", new_premisse);
+                                        Ajout_proposition(r, new_premisse);
+                                        printf("Premisse ajoutee.\n");
+                                    }
+                                    else if (choix == 's'){
+                                        printf("quelle premisse voulez-vous supprimer?\n");
+                                        char del_premisse[100];
+                                        scanf("%s", del_premisse);
+                                        Supprimer_proposition(r, del_premisse);    /// a verif nouvelle fonction
+                                        printf("Premisse supprimee.\n");
+                                    }
+                                    else {
+                                        printf("Choix invalide.\n");
+                                    }
+                                    printf("Premisse modifiee.\n");
+                                }
+
+                                ///modification de la conclusion
+                                else if (choix == 'c'){
+                                    printf("nouvelle conclusion mettre N pour null / la supprimer\n");
+                                    char new_conclusion[100];
+                                    scanf("%s", new_conclusion);
+                                    if (strcmp(new_conclusion, "N") == 0){
+                                        r->conclusion = NULL;
+                                    }
+                                    else {
+                                        r->conclusion = strdup(new_conclusion);
+                                        free(new_conclusion);
+                                    }
+                                    printf("Conclusion modifiee.\n");
+                                }
+                                else {
+                                    printf("Choix invalide.\n");
+                                }
+
+                                printf("voulez vous poursuivre les modifications sur cette regle?(o: oui n: non)\n");
+                                choix = getchar();
+                                if (choix == 'o'){
+                                    modif = 1;
+                                }
+                                else if (choix == 'n'){
+                                    modif = 0;
+                                }
+                                else {
+                                    printf("Choix invalide.\n");
+                                }
+                            }
+                        }
+                        break;
+
+                    ///suppression d'une regle
+                    case 'c':
+                        printf("quel numero de regle voulez-vous supprimer?\n");
+                        scanf("%d", &num_regle);
+                        if (num_regle > taille || num_regle <= 0){
+                            printf("Numero de regle invalide.\n");
+                            break;
+                        }
+                        else{
+                            supprimer_regle(b, num_regle);            ///fait nouvelle fonction mais verif quand meme
+                            printf("Regle supprimee.\n");
+                        }
+                        break;
+
+                    ///affichage des regles
+                    case 'd':
+                        liste_regles_base(b);
+                        break;
+                    
+                    ///suppression de la base
+                    case 'e':
+                        supr_bc(b);
+                        printf("la base a ete supprimee.\n");
+                        action = 0;
+                        selection = 1;
+                        break;
+
+                    ///sauvegarde des bases
+                    case 'f':
+                        Sauvegarde(bases, chemin);       
+                        printf("Bases sauvegardees.\n");
+                        break;
+
+                    ///moteur d inference
+                    case 'g':
+                        moteur_inference(b);
+                        break;
+
+                    default:
+                        printf("Choix invalide.\n");
+                        break;
+                }
+                action = 0;
+            }
+
+            else if (choix == 'n'){
+                conttinuer = 0;
+                action = 0;
+            }
+
+            else if (choix == 'c'){
+                selection = 1;
+                action = 0;
+            }
+
+            else {
+                printf("Choix invalide.\n");
+            }
+
+        ///sauvegarde des bases avant de quitter
+        printf("voulez vous souvgarder?(o: oui, n: non)\n");
+        choix = getchar();
+        if (choix == 'o'){
+            Sauvegarde(bases, chemin);
+            printf("Bases sauvegardees.\n");
+        }
+        else if (choix == 'n'){
+            printf("Bases non sauvegardees.\n");
+        }
+    }
+    return;
+}
 
 
 
