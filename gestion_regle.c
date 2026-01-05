b3671d5d910cc2004b29efd944992416527427c2
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-05 23:20:01 +0100

    Update gestion_regle.c

--- gestion_regle.c
+++ gestion_regle.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include "main.h"
+gestion_regle.h"
 ///////////////////////////////////////////////////////////////
 //             FONCTIONS DE GESTION DES REGLES               //
 ///////////////////////////////////////////////////////////////
