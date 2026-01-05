499580a23c021551c180691c335a486018a9469e
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-05 18:19:42 +0100

    Update fichier_lecture.h

--- fichier_lecture.h
+++ fichier_lecture.h
@@ -7,3 +7,5 @@
 
 BC *Sauvegarde(BC *base, const char *chemin);          //souvegarde la liste des bases dans un fichier .txt a l'emplacement chemin
 BC *lecture_fichier(const char *chemin);          //lis les bases de connaissances dans le fichier et retourne la tête.
+
+#endif
