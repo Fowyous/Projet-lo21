ef1d333a2ed06e8969b7c979d4eee83a574a2c7c
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-05 23:20:39 +0100

    Update gestion_base.c

--- gestion_base.c
+++ gestion_base.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
+#include "gestion_regle.h"
 
 ///////////////////////////////////////////////////////////////
 //      FONCTIONS DE GESTION DE LA BASE DE CONNAISSANCE      //
