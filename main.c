e3a495022779a98f30143717045abe6f53b59935
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-05 22:57:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -327,61 +327,7 @@ void principal() {
 
 
 
-
-
-
-
-
-
-
-char lire_char(){
-
-
-	char c = getchar();
-
-	return c;
-}
-
-void interface_ajout_regle(){// incomplet in faut la configurer pour ajouter la régle a une base
-        Regle new_regle = Creer_regle();
-        while (1){
-                printf("ajouter une proposition(entrer sans rien ajouter pour passer a l'étape suivante):");
-                char *proposition = (char*) malloc(INPUT_MAX * sizeof(char));
-                fgets(proposition, sizeof(INPUT_MAX), stdin);
-                if(proposition[0] == "\n"){break;}
-                Ajout_proposition(new_regle, proposition);
-        }
-        printf("ajouter une conclusion a la règle");
-        char *conclusion = (char*) malloc(INPUTSMAX * sizeof(char));
-	fgets(conclusion, sizeof(INPUT_MAX), stdin);
-	Conclusion(new_regle, conclusion);
-
-}
-
-void menu_BC(){
-
-}
-void menu_principal(){
-        printf("\033[H\033[J");// vider le terminal
-	printf("choisir une option:\n");
-	printf("a. lister les bases de connaissances.\n");
-	printf("b. créer une base de connaissance.\n");
-	char c = lire_char();
-	switch(c){
-	case('a'):
-		menu_BC();
-		break;
-	case('b'):
-		interface_ajout_regle();
-		break;
-	case('c'):
-		break;//a completer
-	default:
-		printf("mauvais choix."); //a completer il faut refaire la question aprés un input pas bon
-	}
-}
 int main(){
-  menu_principal();
+  principal();
   return 0;
-
 }
