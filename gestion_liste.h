fbfa69198b56e9f19c4c587a0b5009a935913cd8
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-05 20:33:37 +0100

    Create gestion_liste.h

---
+++ gestion_liste.h
@@ -0,0 +1,22 @@
+#ifndef GESTION_LISTE_H
+#define GESTION_LISTE_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Liste_faits {
+    char* fait;
+    int nombre_apparition;
+    struct Liste_faits* next;
+} Liste_faits;
+
+bool est_dans_liste_faits(Liste_faits* liste, char* fait_test);          //Prend en parametre un pointeur vers une liste de faits et une chaine de caractere "fait_test", et retourne vrai si le fait est dans la liste.
+void Ajout_fait(Liste_faits** liste, char* nouveau_fait);          //Prend en parametre un pointeur vers une liste de faits et une chaine de caractere "nouveau_fait", ajoute ce fait ou incremente son nombre d'apparition, et ne retourne rien.
+void afficher_liste_faits(Liste_faits* liste);          //Prend en parametre un pointeur vers une liste de faits et affiche chaque fait dans le terminal, et ne retourne rien.
+void supprimer_fait(Liste_faits** liste, char* fait_a_supprimer);          //Prend en parametre un pointeur vers une liste de faits et une chaine "fait_a_supprimer", supprime ou decremente ce fait, et ne retourne rien.
+bool est_vide(Liste_faits* liste);          //Prend en parametre un pointeur vers une liste de faits et retourne vrai si la liste est vide.
+int taille_faits(Liste_faits* liste);          //Prend en parametre un pointeur vers une liste de faits et retourne le nombre de faits dans celle-ci.
+
+#endif
