a716abedb95e67f265f8b763ce2839f4f027e5e1
DARKWolfsull <lespaquerettesdansleschamps@gmail.com>
2026-01-05 23:36:54 +0100

    Update gestion_liste.h

--- gestion_liste.h
+++ gestion_liste.h
@@ -11,7 +11,7 @@ bool est_dans_liste_faits(Liste_faits* liste, char* fait_test);          //Prend
 void Ajout_fait(Liste_faits** liste, char* nouveau_fait);          //Prend en parametre un pointeur vers une liste de faits et une chaine de caractere "nouveau_fait", ajoute ce fait ou incremente son nombre d'apparition, et ne retourne rien.
 void afficher_liste_faits(Liste_faits* liste);          //Prend en parametre un pointeur vers une liste de faits et affiche chaque fait dans le terminal, et ne retourne rien.
 void supprimer_fait(Liste_faits** liste, char* fait_a_supprimer);          //Prend en parametre un pointeur vers une liste de faits et une chaine "fait_a_supprimer", supprime ou decremente ce fait, et ne retourne rien.
-bool est_vide(Liste_faits* liste);          //Prend en parametre un pointeur vers une liste de faits et retourne vrai si la liste est vide.
+bool est_videliste(Liste_faits* liste);          //Prend en parametre un pointeur vers une liste de faits et retourne vrai si la liste est vide.
 int taille_faits(Liste_faits* liste);          //Prend en parametre un pointeur vers une liste de faits et retourne le nombre de faits dans celle-ci.
 
 #endif
